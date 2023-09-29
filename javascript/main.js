8a21f9e662edc9c5871da827a5a7ea0f139745cd
Leonardo Bani <bani.leonardo.25@gmail.com>
2023-09-29 21:20:38 +0200

    modified:   album.html 	modified:   index.html modified:   javascript/main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,8 @@
-const button = document.getElementById("showmail");
-const mail = "info@leonardobani.it";
 
-button.addEventListener("click", () => {
+
+document.getElementById("showmail").addEventListener("click", () => {
   // Cambia il testo all'interno del messaggio
-  const message = document.getElementById("showmail");
-  message.textContent = "if mail app doesn't open ⬆️";
+  document.getElementById("showmail").textContent = "if mail app doesn't open ⬆️";
 
   document.getElementById("lastname").textContent = "info@leonardobani.it";
 
@@ -12,4 +10,53 @@ button.addEventListener("click", () => {
   const mailto =
     "mailto:info@leonardobani.it";
   window.location.href = mailto;
+});
+
+
+document.getElementsByClassName("showmail")[0].addEventListener("click", () => {
+  // Cambia il testo all'interno del messaggio
+  console.log(1/4);
+  document.getElementsByClassName("showmail")[0].textContent = "if mail app doesn't open ⬆️";
+
+  console.log(2/4);
+
+  document.getElementsByClassName("shower")[0].textContent = "info@leonardobani.it";
+  console.log(3/4);
+  // Apri l'applicazione delle mail
+  const mailto =
+    "mailto:info@leonardobani.it";
+  window.location.href = mailto;
+  console.log(4/4);
+});
+
+document.getElementsByClassName("showmail")[1].addEventListener("click", () => {
+  // Cambia il testo all'interno del messaggio
+  console.log(1/4);
+  document.getElementsByClassName("showmail")[1].textContent = "if mail app doesn't open ⬆️";
+
+  console.log(2/4);
+
+  document.getElementsByClassName("shower")[1].textContent = "info@leonardobani.it";
+  console.log(3/4);
+  // Apri l'applicazione delle mail
+  const mailto =
+    "mailto:info@leonardobani.it";
+  window.location.href = mailto;
+  console.log(4/4);
+});
+
+document.getElementsByClassName("showmail")[3].addEventListener("click", () => {
+  // Cambia il testo all'interno del messaggio
+  console.log(1/4);
+  document.getElementsByClassName("showmail")[3].textContent = "if mail app doesn't open ⬆️";
+
+  console.log(2/4);
+
+  document.getElementsByClassName("shower")[3].textContent = "info@leonardobani.it";
+  console.log(3/4);
+  // Apri l'applicazione delle mail
+  const mailto =
+    "mailto:info@leonardobani.it";
+  window.location.href = mailto;
+  console.log(4/4);
 });
