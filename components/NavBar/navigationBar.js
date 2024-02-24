3646cbf62b1543e82f3e1a0dae0d0fdbc56e45c2
Leonardo Bani <bani.leonardo.25@gmail.com>
2024-02-24 14:12:23 +0100

    ..

--- components/NavBar/navigationBar.js
+++ components/NavBar/navigationBar.js
@@ -1,7 +1,7 @@
-// Carica il contenuto della navbar
 fetch("/components/NavBar/navigationBar.html")
   .then(response => response.text())
   .then(html => {
-    // Inserisci il contenuto della navbar nell'elemento con l'id "navbar"
     document.getElementById("NavBar").innerHTML = html;
+
+    
   });
